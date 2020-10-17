7fd26a40915c8adf4a002194a005d84b9b559af1
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2020-10-17 14:46:47 +0100

    Add basic teleportation script

---
+++ Assets/Scripts/TeleportTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TeleportTest : MonoBehaviour
+{
+    public SteamVR_Action_Boolean uiInteractAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("InteractUI");
+    public SteamVR_Input_Sources hand;
+    public GameObject player;
+    private bool upSideDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (uiInteractAction.GetStateDown(hand))
+        {
+            if (!upSideDown)
+            {
+                player.transform.position += Vector3.up * 100;
+                upSideDown = true;
+            }
+            else
+            {
+                player.transform.position += Vector3.down * 100;
+                upSideDown = false;
+            }
+        }
+    }
+}
