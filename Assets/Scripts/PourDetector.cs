43b80d8cc1cb63c22fa4c30425be6f00ea4f888a
Tomas <tomas.smith98@gmail.com>
2020-11-07 13:49:43 +0000

    Added Base Classes For Pour Effect
    PourDetector and Stream are used to create the effect

---
+++ Assets/Scripts/PourDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public float pourThreshold = 35f;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+
+    private void Update()
+    {
+
+    }
+    private void StartPour()
+    {
+
+    }
+
+    private void EndPour()
+    {
+
+    }
+
+    private bool CalculatePourAngle()
+    {
+
+
+    private Stream CreateStream()
+    {
+
+    }
+}
