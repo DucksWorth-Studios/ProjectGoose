b2e3feba99100c63fde3fa668d7d2158746a0650
Tomas <tomas.smith98@gmail.com>
2020-11-07 14:07:42 +0000

    Added Comments to Stream.cs

--- Assets/Scripts/Stream.cs
+++ Assets/Scripts/Stream.cs
@@ -35,37 +35,63 @@ public class Stream : MonoBehaviour
         return null;
     }
     /// <summary>
-    /// Ends stream coming from the tip of the object. the begining of stream will slowly fall down to end point creating a drop effect
+    /// Stop the pouring and begin the ending routine
     /// </summary>
     public void End()
     {
 
     }
 
+    /// <summary>
+    /// Ends stream coming from the tip of the object. the begining of stream will slowly fall down to end point creating a drop effect
+    /// </summary>
     private IEnumerator EndPour()
     {
         return null;
     }
+
+    /// <summary>
+    /// Find the endpoint directly below the origin.
+    /// </summary>
+    /// <returns></returns>
     private Vector3 FindEndpoint()
     {
         return Vector3.zero;
     }
 
+    /// <summary>
+    /// Will be used to update the stream via the linerenderer
+    /// </summary>
+    /// <param name="index">The start or end of the line renderer</param>
+    /// <param name="targetPosition"> Where to move to</param>
     private void MoveToPosition(int index, Vector3 targetPosition)
     {
        
     }
 
+    /// <summary>
+    /// Will be used to update the animation via the linerenderer
+    /// </summary>
+    /// <param name="index">The start or end of the line renderer</param>
+    /// <param name="targetPosition"> Where to animate to</param>
     private void AnimateToPosition(int index, Vector3 targetPosition)
     {
 
     }
-
+    /// <summary>
+    /// Has the start or end of the stream reached the target
+    /// </summary>
+    /// <param name="index">>The start or end of the line renderer</param>
+    /// <param name="targetPosition"> The end position</param>
+    /// <returns>True or False</returns>
     private bool HasReachedPosition(int index, Vector3 targetPosition)
     {
         return false;
     }
 
+    /// <summary>
+    /// If the end of the linerenderer is at the target position create a small splash effect
+    /// </summary>
     private IEnumerator UpdateParticle()
     {
         return null;
