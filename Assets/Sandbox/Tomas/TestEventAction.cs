2c965d76848046993ae6309a571810c02087e13a
Tomas <tomas.smith98@gmail.com>
2020-11-12 16:56:50 +0000

    Added Demo Event Action Script
    Used in sandbox to show events

---
+++ Assets/Sandbox/Tomas/TestEventAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEventAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            EventManager.instance.TestEventCall();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            EventManager.instance.TestEventCallParam(Color.black);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            EventManager.instance.TestEventCallParam(Color.blue);
+        }
+    }
+}
