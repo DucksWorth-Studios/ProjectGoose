98decafdd971e5467c533d971a05bfd28b545673
Tomas <tomas.smith98@gmail.com>
2020-11-12 16:28:32 +0000

    Created Event Manager Class

---
+++ Assets/Sandbox/Tomas/EventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Author: Tomas
+/// Simple Event System for the Game.
+/// Created as a Singleton as it will only need to be created once in the game.
+/// </summary>
+public class EventManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
