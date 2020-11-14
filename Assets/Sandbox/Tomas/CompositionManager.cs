0f5f895c15c096af6d25d30cc483a991a178461c
Tomas <tomas.smith98@gmail.com>
2020-11-14 16:48:33 +0000

    Added in Base Attributes

--- Assets/Sandbox/Tomas/CompositionManager.cs
+++ Assets/Sandbox/Tomas/CompositionManager.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class CompositionManager : MonoBehaviour
 {
+    public Color startColor = Color.white;
+    private Color previousColor = Color.white;
+    private Material currentMaterial = null;
+    private void Awake()
+    {
 
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
 }
