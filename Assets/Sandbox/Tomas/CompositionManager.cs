b6ec647860766b28eecb5154e28fca02b4ecff2d
Tomas <tomas.smith98@gmail.com>
2020-11-16 14:12:33 +0000

    Removed unused code and added comments

--- Assets/Sandbox/Tomas/CompositionManager.cs
+++ Assets/Sandbox/Tomas/CompositionManager.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Author: Tomas
+/// Manages the composition of the vials.
+/// Changes it based on the time shifting and mixing of other chemicals
+/// </summary>
 public class CompositionManager : MonoBehaviour
 {
     public Color currentColor = Color.white;
@@ -12,19 +16,18 @@ public class CompositionManager : MonoBehaviour
     {
         //Get the interactable component
         interactable = GetComponentInParent<Valve.VR.InteractionSystem.Interactable>();
+
         //Allows access to material
         currentMaterial = GetComponent<Renderer>().material;
+
         //Make sure composition matches what its set to.
         currentMaterial.color = currentColor;
+
         //Save what it is currenty/ Wont trigger if they match later
         previousColor = currentColor;
 
+        //Sets the timeSHift method to only call when time is jumped
         EventManager.instance.OnTimeJump += timeShiftChange;
-    }
-
-    void Start()
-    {
-
     }
 
     public void mixChemical(Color chemicalAdditive)
@@ -34,6 +37,7 @@ public class CompositionManager : MonoBehaviour
             //Mix the chemicals by 50%
             currentColor = Color.Lerp(chemicalAdditive,currentColor,0.5f);
             currentMaterial.color = currentColor;
+
             //Ensures only changed once else it will continuesly chnage the material until it becomes the additive
             previousColor = chemicalAdditive;
         }
@@ -51,10 +55,13 @@ public class CompositionManager : MonoBehaviour
 
             //Get the Hue Saturation and Brightness
             Color.RGBToHSV(currentColor, out hue, out saturation, out brightness);
+
             //Modifying the hue by 0.5 will change its color to opposite
             hue += 0.5f;
+
             //Bring it back in range of 0-1
             hue = hue % 1f;
+
             //Recreate the Color
             Color changedColor = Color.HSVToRGB(hue, saturation, brightness);
 
