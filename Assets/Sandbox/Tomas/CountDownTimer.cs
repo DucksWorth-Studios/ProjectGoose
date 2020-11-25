6c4af2ff51f214e428e2f0340b344e293aa91a1d
Tomas <tomas.smith98@gmail.com>
2020-11-25 13:44:25 +0000

    Added Function Delcarations

--- Assets/Sandbox/Tomas/CountDownTimer.cs
+++ Assets/Sandbox/Tomas/CountDownTimer.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Author: Tomas
+/// Used to manage the CountDown of the 
+/// </summary>
 public class CountDownTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float timeAllowed;
+    public bool testBool;
+    private float timeRemaining;
+    private bool isCountingDown = true;
+    private bool isCountingUp = false;
+    private bool isGameOver = false;
     void Start()
     {
-        
+        timeRemaining = timeAllowed;
+        //Events To Subscribe To
     }
 
+    private void CountDown()
+    {
+
+    }
+
+    private void CountUp()
+    {
+    
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if(isCountingDown)
+        {
+            CountDown();
+        }
+        else if(isCountingUp)
+        {
+            CountUp();
+        }
+        else
+        {
+            //Do Nothing
+        }
     }
 }
