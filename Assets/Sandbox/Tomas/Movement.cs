379fd63683a7d9316fb488738d42e06f1364669a
Tomas <tomas.smith98@gmail.com>
2020-11-25 13:19:07 +0000

    Movement Script
    Used to Mock something moving through my trigger

---
+++ Assets/Sandbox/Tomas/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Variables
+    public GameObject cube;
+    private void Awake()
+    {
+        
+    }
+    void Update()
+    {
+        cube.transform.position += new Vector3(0f, 0f, 0.002f);
+    }
+}
+
