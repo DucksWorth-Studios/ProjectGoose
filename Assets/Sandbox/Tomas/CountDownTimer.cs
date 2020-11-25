8cff66631af68d7d43c5eafffaeb7e0ad6a380c3
Tomas <tomas.smith98@gmail.com>
2020-11-25 13:18:46 +0000

    [30289] Set up basic script for Countdown Collision
    Want to detect player when he enters and leaves Via OnCollisionEnter and OnCollisionExit

---
+++ Assets/Sandbox/Tomas/CountDownTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownTimer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("HERE");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //print("Hello");
+    }
+}
