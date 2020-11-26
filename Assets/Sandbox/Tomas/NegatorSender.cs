059598c4118d397174655d350c4194821a00244c
Tomas <tomas.smith98@gmail.com>
2020-11-26 12:56:50 +0000

    Added object Detector for sender

--- Assets/Sandbox/Tomas/NegatorSender.cs
+++ Assets/Sandbox/Tomas/NegatorSender.cs
@@ -5,15 +5,53 @@ using UnityEngine;
 public class NegatorSender : MonoBehaviour
 {
     public GameObject reciever;
+    private Vector3 positionDifference;
+    private GameObject objectInZone;
+    private int entityCount = 0;
     void Start()
     {
-        
+        //Find difference between the two zones
+        Vector3 senderPos = this.transform.position;
+        Vector3 recieverPos = reciever.transform.position;
+        positionDifference = new Vector3(
+          recieverPos.x - senderPos.x,
+          recieverPos.y - senderPos.y,
+          recieverPos.z - senderPos.z);
+
+
+
     }
 
 
     private void SendObject()
     {
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        entityCount++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        entityCount--;
+        if(other.gameObject == objectInZone)
+        {
+            objectInZone = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(entityCount == 0)
+        {
+            objectInZone = other.gameObject;
+        }
+        else if(entityCount == 1 && objectInZone == null)
+        {
+            objectInZone = other.gameObject;
+        }
     }
 
     private bool isObjectInZone()
