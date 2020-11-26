552c5d5d23f89b588c3a4d5ea79631b3fab3be9a
Andrew Carolan <andycarolan9@gmail.com>
2020-11-26 18:54:02 +0000

    [30626] Created Script for collision box
    Setup a script for the collision box to check if the player has entered the collider. If the player has the open doors method is called on the parent game object script

---
+++ Assets/Scripts/Level/Sliding_Doors/CheckCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple Script that checks to see if the player has entered/left the collision box.
+/// Will activate the sliding doors based on the collision
+/// Author: Andrew
+/// </summary>
+public class CheckCollision : MonoBehaviour
+{
+    /// <summary>
+    /// Checks the collision of this collider with any other collider
+    /// If the collider is a player object, the open doors method in the parent script will be called
+    /// </summary>
+    /// <param name="collision">The collider object that collides with this collider</param>
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            transform.GetComponentInParent<SlidingDoors>().OpenDoors();
+    }
+}
