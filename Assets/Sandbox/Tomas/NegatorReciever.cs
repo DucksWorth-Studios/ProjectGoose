f058b0f85a1f3549fb4f6dfc2c534478e417992d
Tomas <tomas.smith98@gmail.com>
2020-11-26 11:04:15 +0000

    [30277] Begun Age Negator
    Added 2 Scripts. Will Be used to communicate across the planes

---
+++ Assets/Sandbox/Tomas/NegatorReciever.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegatorReciever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public bool isOccupied()
+    {
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Sandbox/Tomas/NegatorSender.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NegatorSender : MonoBehaviour
+{
+    public GameObject reciever;
+    void Start()
+    {
+        
+    }
+
+
+    private void SendObject()
+    {
+
+    }
+
+    private bool isObjectInZone()
+    {
+        return false;
+    }
+
+    private bool AreMultipleObjectsInZone()
+    {
+        return false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
