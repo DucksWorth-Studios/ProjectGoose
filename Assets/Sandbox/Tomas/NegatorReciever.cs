7462535488f966c27ceaea027c36d7d84eb22577
Tomas <tomas.smith98@gmail.com>
2020-11-26 12:40:54 +0000

    Added Entity Detector To Reciever

--- Assets/Sandbox/Tomas/NegatorReciever.cs
+++ Assets/Sandbox/Tomas/NegatorReciever.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class NegatorReciever : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int entityCount;
     void Start()
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        entityCount++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        entityCount--;
+    }
     public bool isOccupied()
     {
-        return false;
+        return entityCount == 0;
     }
     // Update is called once per frame
     void Update()
