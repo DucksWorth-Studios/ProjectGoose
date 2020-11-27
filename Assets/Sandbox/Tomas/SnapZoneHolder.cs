5c58dd3c78845b55339fb088fc9b3f3fd1f32c1a
Tomas <tomas.smith98@gmail.com>
2020-11-27 16:18:33 +0000

    Added Snap Zone HOlder
    Item can be taken out of this zone

--- Assets/Sandbox/Tomas/SnapZoneHolder.cs
+++ Assets/Sandbox/Tomas/SnapZoneHolder.cs
@@ -4,12 +4,58 @@ using UnityEngine;
 
 public class SnapZoneHolder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string tagToSearchFor;
+    private GameObject objectCurrentlyHeld;
+    public Vector3 rotation;
+    private bool isHolding = false;
     void Start()
     {
         
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        print("Hello"+other.gameObject.tag);
+        //print("SearchFor" + tagToSearchFor);
+        if(objectCurrentlyHeld == null && other.gameObject.tag == tagToSearchFor)
+        {
+            print("Set");
+            Valve.VR.InteractionSystem.Interactable interactable = other.gameObject.GetComponent< Valve.VR.InteractionSystem.Interactable> ();
+            if(interactable.attachedToHand == null)
+            {
+                print("Hello World");
+                objectCurrentlyHeld = other.gameObject;
+                objectCurrentlyHeld.transform.position = this.transform.position;
+                objectCurrentlyHeld.transform.rotation = Quaternion.Euler(rotation);
+            }
+        }
+        else if (objectCurrentlyHeld == other.gameObject && other.gameObject.transform.rotation != Quaternion.Euler(rotation))
+        {
+            print("Hello My Child");
+            Valve.VR.InteractionSystem.Interactable interactable = other.gameObject.GetComponent<Valve.VR.InteractionSystem.Interactable>();
+            if (interactable.attachedToHand == null)
+            {
+                objectCurrentlyHeld.transform.rotation = Quaternion.Euler(rotation);
+            }
+        }
+        else if (objectCurrentlyHeld == other.gameObject && other.gameObject.transform.position != this.transform.position)
+        {
+            Valve.VR.InteractionSystem.Interactable interactable = other.gameObject.GetComponent<Valve.VR.InteractionSystem.Interactable>();
+            if (interactable.attachedToHand == null)
+            {
+                objectCurrentlyHeld.transform.position = this.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == objectCurrentlyHeld)
+        {
+            isHolding = false;
+            objectCurrentlyHeld = null;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
