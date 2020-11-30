5ee641f30b28025864c40a49687c94cccd08a4f5
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2020-11-30 20:40:53 +0000

    [32243] Find sweet spot for player movement speed

--- Assets/Scripts/VR/ContinuousMovement.cs
+++ Assets/Scripts/VR/ContinuousMovement.cs
@@ -9,7 +9,7 @@ using Valve.VR.InteractionSystem;
 public class ContinuousMovement : MonoBehaviour
 {
     public SteamVR_Action_Vector2 input;
-    public float speed = 1;
+    public float speed = 2.5f;
     private CharacterController characterController;
 
     void Start()
@@ -20,8 +20,7 @@ public class ContinuousMovement : MonoBehaviour
             Debug.LogError("ContinuousMovement.cs is missing input", this);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Prevent locomotion interfering with teleportation
         if (input.axis.magnitude > 0.1f)
