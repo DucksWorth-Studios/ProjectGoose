e8089d36a96f1b30de9c32ca48d633db5a723163
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2020-12-04 00:07:51 +0000

    [30742] Attempt to use material swapping to render outline

---
+++ Assets/Sandbox/Cameron/Scripts/LaserPointerReciever.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class LaserPonterReciever : MonoBehaviour
+{
+    private Color defaultColour = Color.white;
+    public Color hitColour = Color.red;
+    public Color clickColour = Color.green;
+
+    private MeshRenderer meshRenderer;
+
+    public Material defaultMaterial;
+    public Material outlineMaterial;
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultMaterial = meshRenderer.materials[0];
+        defaultColour = meshRenderer.material.color;
+
+        outlineMaterial = (Material)Resources.Load("Materials/Outline", typeof(Material));
+
+        if (!outlineMaterial)
+            Debug.LogError("Failed to load Outline material from resources folder");
+        else
+            outlineMaterial.mainTexture = defaultMaterial.mainTexture;
+    }
+
+    public void HitByRay()
+    {
+        meshRenderer.material = outlineMaterial;
+    }
+    
+    public void RayExit()
+    {
+        meshRenderer.material = defaultMaterial;
+        meshRenderer.material.color = defaultColour;
+    }
+
+    public void Click()
+    {
+        meshRenderer.material.color = clickColour;
+    }
+}
