5e50e017e84ec000455645087da99803017efda9
Tomas <tomas.smith98@gmail.com>
2020-12-06 15:52:57 +0000

    Added Microwave Zones

---
+++ Assets/Sandbox/Tomas/demobutton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demobutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        EventManager.instance.NegatorItemJump();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
