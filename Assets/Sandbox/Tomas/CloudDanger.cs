5c30b818767f33d96e2c86b39d4edb7800822571
Tomas <tomas.smith98@gmail.com>
2020-12-11 12:09:37 +0000

    [32129] Created Danger Script

---
+++ Assets/Sandbox/Tomas/CloudDanger.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudDanger : MonoBehaviour
+{
+    public bool IsPlayerInCloud = false;
+    private bool isGameOver = false;
+    public float timeTosurvive;
+    public float timeRemaining;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsPlayerInCloud == false && other.gameObject.tag == "Player")
+        {
+            IsPlayerInCloud = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(IsPlayerInCloud && other.gameObject.tag == "Player")
+        {
+            IsPlayerInCloud = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsPlayerInCloud && !isGameOver)
+        {
+            startCountdown();
+        }
+        else if(!IsPlayerInCloud && !isGameOver)
+        {
+            resetCountDown();
+        }
+    }
+
+    private void resetCountDown()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void startCountdown()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+
+        }
+    }
+}
