cffb2f414796d603ad11279e3e17796b89039746
Tomas <tomas.smith98@gmail.com>
2020-12-13 15:30:26 +0000

    [30571] Created Part of Handler

---
+++ Assets/Sandbox/Tomas/ItemHolder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHolder : MonoBehaviour
+{
+    public GameObject objectToHold;
+    public bool isHolding = true;
+    public bool keepHolding = true;
+    void Start()
+    {
+        KeepObjectInZone();
+        //Set as not interactable
+    }
+    public void setInteractable()
+    {
+        //set interactable
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == objectToHold)
+        {
+            isHolding = false;
+            objectToHold = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeepObjectInZone();
+    }
+    //Ensures Object stays in zone.
+    private void KeepObjectInZone()
+    {
+        if(objectToHold.transform.position != this.transform.position)
+        {
+            objectToHold.transform.position = this.transform.position;
+        }
+        if(objectToHold.transform.rotation != this.transform.rotation)
+        {
+            objectToHold.transform.rotation = this.transform.rotation;
+        }
+    }
+}
