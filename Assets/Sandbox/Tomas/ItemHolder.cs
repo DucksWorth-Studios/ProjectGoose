2ca1d375a24d9e3fe1454311c2549632b15b873b
Tomas <tomas.smith98@gmail.com>
2020-12-14 12:32:04 +0000

    [30571] Adjusted Item Holder
    Better Detects when items leave or stay in their zones

--- Assets/Sandbox/Tomas/ItemHolder.cs
+++ Assets/Sandbox/Tomas/ItemHolder.cs
@@ -2,7 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Author:Tomas
+/// Keeps Items in a zone until further notice
+/// </summary>
 public class ItemHolder : MonoBehaviour
 {
     public GameObject objectToHold;
@@ -10,19 +13,25 @@ public class ItemHolder : MonoBehaviour
     public bool keepHolding = true;
     void Start()
     {
+        //Set to SnapZones
         KeepObjectInZone();
-        //Set as not interactable
+        //Set as not interactable not currently working
         objectToHold.GetComponent<Valve.VR.InteractionSystem.Interactable>().enabled = false;
+        objectToHold.GetComponent<VRItemAttachment>().enabled = false;
     }
+
+    //Allow Pickup
     public void setInteractable()
     {
         //set interactable
         objectToHold.GetComponent<Valve.VR.InteractionSystem.Interactable>().enabled = true;
+        objectToHold.GetComponent<VRItemAttachment>().enabled = true;
+        keepHolding = false;
     }
-
+    //Unset object from zone if meets criteria
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject == objectToHold)
+        if (other.gameObject == objectToHold && keepHolding == false)
         {
             isHolding = false;
             objectToHold = null;
@@ -39,11 +48,11 @@ public class ItemHolder : MonoBehaviour
     {
         if(objectToHold != null)
         {
-            if (objectToHold.transform.position != this.transform.position)
+            if (objectToHold.transform.position != this.transform.position && objectToHold.GetComponent< Valve.VR.InteractionSystem.Interactable>().attachedToHand ==null)
             {
                 objectToHold.transform.position = this.transform.position;
             }
-            if (objectToHold.transform.rotation != this.transform.rotation)
+            if (objectToHold.transform.rotation != this.transform.rotation && objectToHold.GetComponent<Valve.VR.InteractionSystem.Interactable>().attachedToHand == null)
             {
                 objectToHold.transform.rotation = this.transform.rotation;
             }
