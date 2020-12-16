e57bef21538e374d4c3bc3344b70e1b9df8d9589
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2020-12-16 22:31:07 +0000

    [30734] Adjust effect and update player prefab

--- Assets/Sandbox/Cameron/Scripts/FadeCamera.cs
+++ Assets/Sandbox/Cameron/Scripts/FadeCamera.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Valve.VR;
 
+/// <summary>
+/// Script to fadeout camera view when a collision is detected.
+/// Based on:
+/// https://forum.unity.com/threads/head-collider-steam-vr-plugin.805743/
+/// https://github.com/ValveSoftware/steamvr_unity_plugin/issues/388
+/// </summary>
 public class FadeCamera : MonoBehaviour
 {
-    private float _fadeDuration = 0f;
+    private float _fadeDuration = 0.1f;
  
     private void Start()
     {
@@ -14,24 +18,24 @@ public class FadeCamera : MonoBehaviour
     }
     private void FadeToWhite()
     {
-        Debug.Log("FadeToWhite");
+        // Debug.Log("FadeToWhite");
         //set start color
         SteamVR_Fade.View(Color.clear, 0f);
         //set and start fade to
-        SteamVR_Fade.View(Color.black, _fadeDuration);
+        SteamVR_Fade.View(Color.white, _fadeDuration);
     }
     private void FadeFromWhite()
     {
-        Debug.Log("FadeFromWhite");
+        // Debug.Log("FadeFromWhite");
         //set start color
-        SteamVR_Fade.View(Color.black, 0f);
+        SteamVR_Fade.View(Color.white, 0f);
         //set and start fade to
         SteamVR_Fade.View(Color.clear, _fadeDuration);
     }
  
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TriggerEnter: VRCamera");
+        // Debug.Log("TriggerEnter: VRCamera");
         FadeToWhite();
     }
  
