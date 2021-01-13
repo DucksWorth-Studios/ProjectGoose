446c792d946b50b1736c93091b5d949a8904976e
Tomas <tomas.smith98@gmail.com>
2021-01-13 12:27:28 +0000

    [30567] Moved Snap Zone to Production

--- Assets/Scripts/SnapZonePermanent.cs
+++ Assets/Scripts/SnapZonePermanent.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum Snap { USB };
 /// <summary>
 /// Author: Tomas
 /// Snap zones will be used for keys and other objects. When an item enters designated zone it will snap to position triggering an Event.
 /// </summary>
-public class SnapZonePermenant : MonoBehaviour
+public class SnapZonePermanent : MonoBehaviour
 {
     [Tooltip("The Object that will Snap here")]
     public GameObject objectToSnap;
 
     [Tooltip("The Rotation object should stay in")]
     public Vector3 rotation;
+
+    [Tooltip("Event The Object Should Send")]
+    public Snap itemSnapped;
     //string eventToCall might be used to differentiate events
     private bool isSnapped = false;
 
@@ -25,7 +30,7 @@ public class SnapZonePermenant : MonoBehaviour
             objectToSnap.transform.position = this.transform.position;
 
             //Send event
-
+            EventManager.instance.SnappedItem(itemSnapped);
         }
     }
 }
