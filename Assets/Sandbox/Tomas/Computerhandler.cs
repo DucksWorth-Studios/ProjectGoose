846597b1181b1ecfea14aa09d86bfe3eaca5c118
Tomas <tomas.smith98@gmail.com>
2021-01-13 12:42:16 +0000

    [30567] Added Computer Handler Script

---
+++ Assets/Sandbox/Tomas/Computerhandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Computerhandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image loadingScreen;
+    public Image instructions;
+    void Start()
+    {
+        EventManager.instance.OnItemSnap += enableScreen;
+    }
+
+    private void enableScreen(Snap itemSnapped)
+    {
+        if(itemSnapped == Snap.USB)
+        {
+            loadingScreen.enabled = true;
+            //StartCoRoutine
+            //SOUND
+        }
+    }
+
+    IEnumerator beginLoading()
+    {
+        yield return null;
+    }
+}
