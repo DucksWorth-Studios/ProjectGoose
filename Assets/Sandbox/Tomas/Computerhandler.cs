efba08dac8ecc847b92cb3241e9807c9c4e41ab7
Tomas <tomas.smith98@gmail.com>
2021-01-14 12:03:04 +0000

    [30567] Added functionality to manage Images

--- Assets/Sandbox/Tomas/Computerhandler.cs
+++ Assets/Sandbox/Tomas/Computerhandler.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+/// <summary>
+/// Author: Tomas
+/// Used to control the computer loading the formula
+/// </summary>
 public class Computerhandler : MonoBehaviour
 {
     // Start is called before the first frame update
     public Image loadingScreen;
     public Image instructions;
+    public int loadTime;
+    private float passedTime;
+
     void Start()
     {
         EventManager.instance.OnItemSnap += enableScreen;
@@ -16,14 +24,34 @@ public class Computerhandler : MonoBehaviour
     {
         if(itemSnapped == Snap.USB)
         {
-            loadingScreen.enabled = true;
-            //StartCoRoutine
+            print("Hello");
+            loadingScreen.gameObject.SetActive(true);
             //SOUND
         }
     }
 
-    IEnumerator beginLoading()
+    private void loading()
     {
-        yield return null;
+        if(loadingScreen.gameObject.activeSelf)
+        {
+            passedTime += Time.deltaTime;
+            loadingScreen.fillAmount = passedTime / loadTime;
+            if (passedTime >= loadTime)
+            {
+                displayFormula();
+            }
+        }
     }
+
+    private void displayFormula()
+    {
+        instructions.gameObject.SetActive(true);
+        loadingScreen.gameObject.SetActive(false);
+        //SOUND
+    }
+    void Update()
+    {
+        loading();
+    }
+
 }
