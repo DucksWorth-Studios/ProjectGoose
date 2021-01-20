34442281d50e4ee40779571be456f43e57303fe1
Andrew Carolan <andycarolan9@gmail.com>
2021-01-20 23:34:18 +0000

    [30731] Added Hand Over Update
    added code for when the player is grabbing the door, so that the force they are applying can be calculated in the update function

--- Assets/Scripts/Level/Sliding_Doors/BrokenSlidingDoors.cs
+++ Assets/Scripts/Level/Sliding_Doors/BrokenSlidingDoors.cs
@@ -15,16 +15,23 @@ public class BrokenSlidingDoors : MonoBehaviour
     public Transform StartPosition;
     public Transform EndPosition;
 
+    private Vector3 handStartGrabPos;
+
     private Hand.AttachmentFlags attachmentFlags = Hand.defaultAttachmentFlags & (~Hand.AttachmentFlags.SnapOnAttach) & (~Hand.AttachmentFlags.DetachOthers) & (~Hand.AttachmentFlags.VelocityMovement);
 
     private Rigidbody rigidbody;
     private Interactable interactable;
 
+    private bool isAttached = false;
+
+    [Tooltip("Hand needed for force calculations")]
+    private Hand hand;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (this.transform != StartPosition)
-            this.transform.position = StartPosition.position;
+        //if (this.transform != StartPosition)
+        //    this.transform.position = StartPosition.position;
 
         rigidbody = GetComponent<Rigidbody>();
         interactable = GetComponent<Interactable>();
@@ -33,11 +40,33 @@ public class BrokenSlidingDoors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isAttached)
+        {
+            ///TODO add code for calculating force
+        }
     }
 
     private void HandHoverUpdate(Hand hand)
     {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(gameObject);
 
+        if(interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            hand.HoverLock(interactable);
+
+            //hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
+
+            this.hand = hand;
+            handStartGrabPos = hand.transform.position;
+            isAttached = true;
+        }
+        else if(isGrabEnding)
+        {
+            //hand.DetachObject(gameObject);
+            isAttached = false;
+
+            hand.HoverUnlock(interactable);
+        }
     }
 }
