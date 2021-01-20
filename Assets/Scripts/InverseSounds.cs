a7d877e66abec674c7fdfa7d01a2be4d9d01572f
Tomas <tomas.smith98@gmail.com>
2021-01-20 11:44:33 +0000

    [30437] Added InverSound Manager

---
+++ Assets/Scripts/InverseSounds.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Author Tomas
+/// This is used where a sound has two  potential outccomes
+ /// </summary>
+public class InverseSounds : MonoBehaviour
+{
+    public AudioClip clipOne;
+    public AudioClip clipTwo;
+    public Sound soundToPlay;
+    private AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        EventManager.instance.OnPlayOneSound += PlaySound;
+    }
+
+    private void PlaySound(Sound sound,bool firstSound)
+    {
+        if(sound == soundToPlay)
+        {
+            if(firstSound && clipOne != null)
+            {
+                source.clip = clipOne;
+                source.Play();
+            }
+
+            if(!firstSound && clipTwo != null)
+            {
+                source.clip = clipTwo;
+                source.Play();
+            }
+        }
+    }
+}
