6526dcfde210f4008ae756aee8b9ba2853b933b5
Tomas <tomas.smith98@gmail.com>
2021-01-21 12:57:05 +0000

    [30612] Added Scenes to progression

--- Assets/Sandbox/Tomas/ProgressionManager.cs
+++ Assets/Sandbox/Tomas/ProgressionManager.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Progression { };
+public enum STAGE { START, FIRSTJUMP, FIRSTRETURN, USB, USBPLUGGED, CHEMICALPUZZLE, ELEMENTPUZZLE,END };
 public class ProgressionManager : MonoBehaviour
 {
+    public NarrationManager narrationManager;
+    public static ProgressionManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +19,74 @@ public class ProgressionManager : MonoBehaviour
 
     //Outside forces dictate what stage it is at.
     //It then calls up the relevant stage
-    public void Progress(Progression stage)
+    public void Progress(STAGE stage)
     {
         switch(stage)
         {
+            case STAGE.START:
+                GameStart();
+                break;
+            case STAGE.FIRSTJUMP:
+                FirstJump();
+                break;
+            case STAGE.FIRSTRETURN:
+                FirstReturn();
+                break;
+            case STAGE.USB:
+                USB();
+                break;
+            case STAGE.USBPLUGGED:
+                USBPlugged();
+                break;
+            case STAGE.CHEMICALPUZZLE:
+                ChemicalPuzzle();
+                break;
+            case STAGE.ELEMENTPUZZLE:
+                ElementPuzzle();
+                break;
+            case STAGE.END:
+                GameEnd();
+                break;
             default:
                 break;
         }
     }
 
-    private void StageOne()
+    private void GameStart()
+    {
+
+    }
+
+    private void FirstJump()
+    {
+
+    }
+
+    private void FirstReturn()
+    {
+
+    }
+
+    private void USB()
+    {
+
+    }
+
+    private void USBPlugged()
+    {
+
+    }
+
+    private void ChemicalPuzzle()
+    {
+
+    }
+    private void ElementPuzzle()
+    {
+
+    }
+
+    private void GameEnd()
     {
 
     }
