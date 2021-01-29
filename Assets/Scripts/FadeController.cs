5d34af4addf5df6fb576b8a4468dbc03aa9d417a
Tomas <tomas.smith98@gmail.com>
2021-01-29 13:35:21 +0000

    [30969] Added Comments To Fade

--- Assets/Scripts/FadeController.cs
+++ Assets/Scripts/FadeController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Author: Tomas
+/// Used To Control Fade Outs And Fade Ins
+/// </summary>
 public class FadeController : MonoBehaviour
 {
-    public float timeOut = 5f;
+    public float timeOut = 10f;
     public Color fadeColor = Color.black;
+
     void Start()
     {
         EventManager.instance.OnFadeScreen += FadeEvent;
-        //Valve.VR.SteamVR_Fade.View(Color.black, 0);
     }
 
-
+    /// <summary>
+    /// The Main event. the bool passed toggles wether its fadeout event or not if not a fade in event occurs.
+    /// </summary>
+    /// <param name="fadeout"></param>
     private void FadeEvent(bool fadeout)
     {
         if(fadeout)
@@ -21,8 +27,8 @@ public class FadeController : MonoBehaviour
             Valve.VR.SteamVR_Fade.View(fadeColor,timeOut);
         }
         else
-        {
-            Valve.VR.SteamVR_Fade.View(fadeColor, timeOut);
+        {  
+            Valve.VR.SteamVR_Fade.View(fadeColor, 0);
             Valve.VR.SteamVR_Fade.View(Color.clear, timeOut);
         }
     }
@@ -31,5 +37,6 @@ public class FadeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
