990c527933e7c3918c7dc2bf4a1a61222af83e70
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2021-01-31 22:11:52 +0000

    [30633]Experiment with Steam VR Laser Pointer

---
+++ Assets/Scripts/VR/SVRPointerReciever.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR;
+using Valve.VR.Extras;
+
+public class SVRPointerReciever : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public SteamVR_LaserPointer laserPointer;
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was clicked");
+        } else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was clicked");
+        }
+        else
+        {
+            Debug.Log(e.target.name + " Clicked");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was entered");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was entered");
+        }
+        else
+        {
+            Debug.Log(e.target.name + " Entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "Cube")
+        {
+            Debug.Log("Cube was exited");
+        }
+        else if (e.target.name == "Button")
+        {
+            Debug.Log("Button was exited");
+        }
+        else
+        {
+            Debug.Log(e.target.name + "Exited");
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
