0325c76730c3780bd706b80f88c4d4731f07b3fc
Tomas <tomas.smith98@gmail.com>
2021-02-01 12:43:24 +0000

    [30284] Defined Functions

--- Assets/Scripts/KeyItem.cs
+++ Assets/Scripts/KeyItem.cs
@@ -11,6 +11,8 @@ public enum KEY { CHEMICAL,USB,USBSLOT };
 [RequireComponent(typeof(Outline))]
 public class KeyItem : MonoBehaviour
 {
+    [Tooltip("Select To Differentiate it for the event manager")]
+    public KEY itemToListen;
     //Was the Item Handled
     private bool WasHandled = false;
     //Is the Item Highlighted
@@ -24,21 +26,35 @@ public class KeyItem : MonoBehaviour
     void Start()
     {
         outline = GetComponent<Outline>();
+        outline.enabled = false;
         interactable = GetComponent<Valve.VR.InteractionSystem.Interactable>();
+        EventManager.instance.OnItemHighlight += TurnOnHighLight;
     }
 
-    private void TurnOnHighLight()
+    private void TurnOnHighLight(KEY item)
     {
-
+        if(item == itemToListen)
+        {
+            outline.enabled = true;
+            IsHighlighted = true;
+        }
     }
 
-    private void TurnOffHighLight()
+    private void CheckToTurnOFF()
     {
-
+        if (interactable.attachedToHand != null)
+        {
+            IsHighlighted = false;
+            outline.enabled = false;
+        }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsHighlighted)
+        {
+            CheckToTurnOFF();
+        }
     }
 }
