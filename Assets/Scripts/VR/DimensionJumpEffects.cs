1a24306ed60f8efc9a8fd56651f29f29f2de59b0
Andrew Carolan <andycarolan9@gmail.com>
2021-02-04 16:53:06 +0000

    [30933] Implemented Camera fade Event Function
    Created function that fades camera to a black screen then back to clear after 0.5 seconds

--- Assets/Scripts/VR/DimensionJumpEffects.cs
+++ Assets/Scripts/VR/DimensionJumpEffects.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class DimensionJumpEffects : MonoBehaviour
 {
@@ -18,5 +19,31 @@ public class DimensionJumpEffects : MonoBehaviour
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Fades the camera to black then back to clear for the dimension jump
+    /// Based off Cameron's fade to white method in FadeCamera Class
+    /// </summary>
+    /// <see cref="FadeCamera.FadeToWhite"/>
+    /// <returns>a yield return that waits for 0.5 seconds to allow for fade of camera</returns>
+    private IEnumerator StartCameraFade()
+    {
+        //set start color
+        SteamVR_Fade.View(Color.clear, 0f);
+
+        //fade to black
+        SteamVR_Fade.View(fadeColor, fadeDuration);
+
+        //Wait for the fade to complete
+        yield return new WaitForSeconds(0.5f);
+
+        //fade back to clear
+        SteamVR_Fade.View(Color.clear, fadeDuration);
+    }
+
+    private void StartVFX()
+    {
+
     }
 }
