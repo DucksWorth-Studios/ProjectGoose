39b6429d6d7d33125099624ca565ccc48419a820
Andrew Carolan <andycarolan9@gmail.com>
2021-02-15 21:26:23 +0000

    [33658] Reset Time Elapsed Variable On Complete
    Reset the float variable back to 0 for the next time the object has to be decayed

--- Assets/Scripts/Level/Interactable/DecayMaterial.cs
+++ Assets/Scripts/Level/Interactable/DecayMaterial.cs
@@ -9,8 +9,6 @@ using Valve.VR.InteractionSystem;
 /// </summary>
 public class DecayMaterial : MonoBehaviour
 {
-    private const float FULL_OPACITY = 1.0f;
-
     [Tooltip("Interactable script in the parent gameobject to check if this object is being held at the time of dimension jump")]
     private Interactable interactable;
 
@@ -23,73 +21,6 @@ public class DecayMaterial : MonoBehaviour
     [Tooltip("Get the mesh renderer to change the materials of the object")]
     private MeshRenderer renderer;
 
-    #region V1
-    //private float timeSinceJump = 0.0f;
-
-    //public Material baseMat;
-    //public Material decayedMat;
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    if (this.transform.parent != null)
-    //        interactable = GetComponentInParent<Interactable>();
-    //    else
-    //        interactable = GetComponent<Interactable>();
-
-    //    renderer = GetComponent<MeshRenderer>();
-
-    //    if (!isDecayed)
-    //        renderer.material = baseMat;
-    //    else
-    //        renderer.material = decayedMat;
-
-    //    EventManager.instance.OnTimeJump += StartMaterialDecay;
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (isDecaying)
-    //        DecayMaterials();
-    //}
-
-    private void StartMaterialDecay()
-    {
-        if (interactable.attachedToHand != null) // Object is in the hand of the player
-            isDecaying = true;
-    }
-
-    ///// <summary>
-    ///// Will wait till half a second after the player has jumped to the decayed world and change the texture of the object to the decayed material
-    ///// TODO Add a VFX to make swap cleaner
-    ///// </summary>
-    //private void DecayMaterials()
-    //{
-    //    timeSinceJump += 0.5f * Time.deltaTime;
-
-    //    if (timeSinceJump < 0.5f)
-    //        return;
-
-    //    if (!isDecayed)
-    //    {
-    //        renderer.material = decayedMat;
-    //    }
-    //    else
-    //    {
-    //        renderer.material = baseMat;
-    //    }
-
-    //    if (timeSinceJump > FULL_OPACITY)
-    //    {
-    //        isDecayed = isDecayed != true ? true : false;
-    //        isDecaying = false;
-    //        timeSinceJump = 0;
-    //    }
-    //}
-    #endregion
-
-    #region V2
     Material material;
     float blendAmount = 0;
     float timeElasped;
@@ -106,6 +37,8 @@ public class DecayMaterial : MonoBehaviour
         renderer = GetComponent<MeshRenderer>();
 
         material = renderer.material;
+
+        EventManager.instance.OnTimeJump += StartMaterialDecay;
     }
 
     private void Update()
@@ -127,8 +60,15 @@ public class DecayMaterial : MonoBehaviour
             {
                 isDecayed = (blendAmount >= 0.95f) ? true : false;
                 isDecaying = false;
+
+                timeElasped = 0;
             }
         }
     }
-    #endregion
+
+    private void StartMaterialDecay()
+    {
+        if (interactable.attachedToHand != null) // Object is in the hand of the player
+            isDecaying = true;
+    }
 }
