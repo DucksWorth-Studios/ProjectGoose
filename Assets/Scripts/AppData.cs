b902ff7ba18712b34ec845921246fd8508c8964e
Tomas <tomas.smith98@gmail.com>
2021-02-15 12:27:36 +0000

    [33296] Added Enums to Appdata

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -1,6 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Used To play Sounds
+public enum Sound { DoorSound, Creaking, Breaking, Alarm, Airlock, Walk, Teleport, ItemPickUp, USB, ItemTeleport };
+
+//Used To Define the Stage
+public enum STAGE { START, FIRSTJUMP, FIRSTRETURN, USB, USBPLUGGED, CHEMICALPUZZLE, ELEMENTPUZZLE, END, USBRETURN };
+
+//Used For A Snap Event
+public enum Snap { USB };
+
+//Key Enum Used TO Control what Items Highlight
+public enum KEY { CHEMICAL, USB, USBSLOT };
+
+//Used Define the scene to play
+public enum SCENE { ONE, TWO, THREE, FOUR, FIVE, SIX, FOURP2 };
 
 public class AppData : MonoBehaviour
 {
