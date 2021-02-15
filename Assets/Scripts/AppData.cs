4529881faa4354fa1dc7d52c23be214366862074
Tomas <tomas.smith98@gmail.com>
2021-02-15 12:55:01 +0000

    [33296] Added Floats and Strings

--- Assets/Scripts/AppData.cs
+++ Assets/Scripts/AppData.cs
@@ -16,7 +16,21 @@ public enum KEY { CHEMICAL, USB, USBSLOT };
 //Used Define the scene to play
 public enum SCENE { ONE, TWO, THREE, FOUR, FIVE, SIX, FOURP2 };
 
+//Button Enum
+public enum ButtonEnum { CLOUDREMOVE, NEGATOR };
+
 public class AppData : MonoBehaviour
 {
+    /*---Floats---*/
+    public static float fadeTimeOut = 10f;
+    public static float cloudSurviveTime = 27f;
+    // Delay from survival to item destruction
+    public static float cloudSurviveTimeDelay = 3f;
 
+
+    /*---Strings---*/
+    public static string ignoreTag = "Ignore";
+    public static string elementTag = "Element";
+    public static string chemicalTag = "Chemical";
+    public static string playerTag = "Player";
 }
