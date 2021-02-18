a5ec21d2be1f10ba261154ed7de37c3e0a1c0224
Andrew Carolan <andycarolan9@gmail.com>
2021-02-18 18:09:17 +0000

    [33649] Created Retort Stand Snap Zone Script
    Will snap the test tube prefab to the claws of the retort stand

---
+++ Assets/Scripts/RetortStandSnapZone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetortStandSnapZone : MonoBehaviour
+{
+    [Tooltip("Tag To Search For")]
+    public string tagToSearchFor;
+
+    [Tooltip("The Roation of The Snapped Object")]
+    public Vector3 rotation;
+
+    private GameObject objectCurrentlyHeld;
+    private bool isHolding = false;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(objectCurrentlyHeld == null && other.gameObject.tag == tagToSearchFor)
+        {
+            other.gameObject.GetComponent<Throwable>().ToggleGravity();
+
+            objectCurrentlyHeld = other.gameObject;
+            objectCurrentlyHeld.transform.position = this.transform.position;
+            objectCurrentlyHeld.transform.rotation = Quaternion.Euler(rotation);
+
+            isHolding = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == objectCurrentlyHeld && other.gameObject.GetComponent<Valve.VR.InteractionSystem.Interactable>().attachedToHand != null)
+        {
+            isHolding = false;
+            objectCurrentlyHeld = null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHolding)
+        {
+            objectCurrentlyHeld.transform.position = this.transform.position;
+            objectCurrentlyHeld.transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+}
