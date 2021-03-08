5e469a637aa9f74ae4fcc8873cb395059b5128ad
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2021-03-08 10:43:05 +0000

    Set default pointer state to be not russels

--- Assets/SteamVR/InteractionSystem/Core/Scripts/Custom/EventManager.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/Custom/EventManager.cs
@@ -273,7 +273,7 @@ public class EventManager : MonoBehaviour
         DisablePointer();
     }
     
-    public virtual void EnableAllInput(PointerState state = PointerState.PhysicsPointer)
+    public virtual void EnableAllInput(PointerState state = PointerState.NotRussels)
     {
         //TODO: Change to enable physics by default
         EnableMovement();
