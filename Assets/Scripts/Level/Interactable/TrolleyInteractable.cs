bc1c5f47ebfc5da443c45c7cbc2f52c1666b4604
Andrew Carolan <andycarolan9@gmail.com>
2021-03-13 00:31:09 +0000

    [33712] Handled Trolley Rotation
    Rotate the trolley based on the angle between the previous y rotation of the hand holding the trolley and the current y rotation of the hand

--- Assets/Scripts/Level/Interactable/TrolleyInteractable.cs
+++ Assets/Scripts/Level/Interactable/TrolleyInteractable.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(Interactable))]
+[RequireComponent(typeof(Rigidbody))]
 public class TrolleyInteractable : MonoBehaviour
 {
     private Interactable interactable;
 
     private Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.VelocityMovement
+                                                    & ~Hand.AttachmentFlags.SnapOnAttach
                                                     & ~Hand.AttachmentFlags.DetachOthers
                                                     & ~Hand.AttachmentFlags.DetachFromOtherHand;
+
+    private Hand attachedHand;
+    private float previousRot;
+    private Rigidbody rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         interactable = GetComponent<Interactable>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        if(attachedHand != null)
+        {
+            if(previousRot == 0)
+            {
+                previousRot = attachedHand.transform.eulerAngles.y;
+                return;
+            }
+
+            var angle = Vector3.Angle(new Vector3(0, previousRot, 0), new Vector3(0, previousRot = attachedHand.transform.eulerAngles.y, 0));
+
+            this.transform.Rotate(0, angle, 0);
+
+            ///Force Rotation only to move in the y axis
+            rigidbody.angularVelocity = Vector3.zero;
+            this.transform.eulerAngles = new Vector3(0, this.transform.eulerAngles.y, 0);
+        }
     }
 
     private void HandHoverUpdate(Hand hand)
@@ -33,11 +55,15 @@ public class TrolleyInteractable : MonoBehaviour
         {
             hand.HoverLock(interactable);
 
+            attachedHand = hand;
+
             hand.AttachObject(gameObject, startingGrabType, attachmentFlags);
         }
         else if(isGrabEnding)
         {
             hand.DetachObject(gameObject);
+
+            attachedHand = null;
 
             hand.HoverUnlock(interactable);
         }
