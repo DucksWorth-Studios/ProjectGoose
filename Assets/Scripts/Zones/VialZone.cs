95455bc0e7e562e192780c68001d8913a609a436
Tomas <tomas.smith98@gmail.com>
2021-03-15 12:16:43 +0000

    [34061] Update Script Name

--- Assets/Scripts/Zones/VialZone.cs
+++ Assets/Scripts/Zones/VialZone.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndZone : MonoBehaviour
+/// <summary>
+/// Author: Tomas
+/// Zone For the Final Composition. Checks if element is in the vial if it is then the composition is correct and allows object to snap
+/// </summary>
+[RequireComponent(typeof(BoxCollider))]
+public class VialZone : MonoBehaviour
 {
     [Tooltip("The Rotation object should stay in")]
     public Vector3 rotation;
 
     //The Composition
     private GameObject solution;
-    //string eventToCall might be used to differentiate events
+    //is object currently Snapped
     private bool isSnapped = false;
 
     private void OnTriggerEnter(Collider other)
     {
+        //Checks If Vial
         if ("Vial" == other.gameObject.tag)
         {
+            //Checks If It Has the Element
             if(other.gameObject.GetComponentInChildren<CompositionManager>().HasElement)
             {
+                //Diasbales Interactions and Detaches From hand
                 isSnapped = true;
                 other.gameObject.GetComponent<Throwable>().attachmentEnabled = false;
                 other.gameObject.GetComponent<Valve.VR.InteractionSystem.Interactable>().attachedToHand.DetachObject(other.gameObject, true);
@@ -26,6 +34,8 @@ public class EndZone : MonoBehaviour
                 other.gameObject.transform.position = this.transform.position;
                 other.gameObject.transform.rotation = Quaternion.Euler(rotation);
                 solution = other.gameObject;
+
+                //Send Game Over Events
                 EventManager.instance.Progress(STAGE.END);
                 EventManager.instance.WinGame();
             }
@@ -34,6 +44,7 @@ public class EndZone : MonoBehaviour
 
     private void Update()
     {
+        //Keeps Items In Zone. Rigidbody ddeeactivation will not disable Gravity
         if (isSnapped)
         {
             solution.transform.position = this.transform.position;
