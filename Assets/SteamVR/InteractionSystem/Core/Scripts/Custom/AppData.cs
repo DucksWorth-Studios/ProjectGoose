4d2aa703ac623a02bccb0a7b614c85fa43d40d0f
Tomas <tomas.smith98@gmail.com>
2021-03-18 11:50:46 +0000

    [33300] Added Timer To Sound Enum

--- Assets/SteamVR/InteractionSystem/Core/Scripts/Custom/AppData.cs
+++ Assets/SteamVR/InteractionSystem/Core/Scripts/Custom/AppData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //Used To play Sounds
-public enum Sound { DoorSound, Creaking, Breaking, Alarm, Airlock, Walk, Teleport, ItemPickUp, USB, ItemTeleport };
+public enum Sound { DoorSound, Creaking, Breaking, Alarm, Airlock, Walk, Teleport, ItemPickUp, USB, ItemTeleport, Timer };
 
 //Used To Define the Stage
 public enum STAGE { START, FIRSTJUMP, FIRSTRETURN, USB, USBPLUGGED, CHEMICALPUZZLE, ELEMENTPUZZLE, END, USBRETURN };
