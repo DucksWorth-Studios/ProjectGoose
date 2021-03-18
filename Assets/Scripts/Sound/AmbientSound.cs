32664d85031f76b12b5efed58dccab5b276f5e21
Tomas <tomas.smith98@gmail.com>
2021-03-18 11:50:18 +0000

    [33300] Added Ambient Controller

--- Assets/Scripts/Sound/AmbientSound.cs
+++ Assets/Scripts/Sound/AmbientSound.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 /// Author: Tomas
 /// Provides Background Sounds that do not conistently play.
 /// </summary>
+[RequireComponent(typeof(AudioSource))]
 public class AmbientSound : MonoBehaviour
 {
     [Tooltip("Audio File To Play")]
     public AudioClip audioClip;
 
-    public float frequency = 0;
+    public float frequency;
 
+    public bool playOnStart;
     private AudioSource source;
     private float timeRemaining;
     private float interval;
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
     void Start()
     {
-        
+        interval = audioClip.length + frequency;
+        timeRemaining = interval;
+        source.clip = audioClip;
+        if(playOnStart)
+        {
+            source.Play();
+        }
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(timeRemaining);
+        timeRemaining -= Time.deltaTime;
+        if(timeRemaining < 0)
+        {
+            timeRemaining = interval;
+            source.Play();
+        }
     }
 }
