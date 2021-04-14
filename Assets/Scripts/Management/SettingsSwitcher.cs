f33039cb1ef060695357518edc6905012fdb378a
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2021-04-14 14:13:07 +0100

    [35193] Add check to hide menus after update

--- Assets/Scripts/Management/SettingsSwitcher.cs
+++ Assets/Scripts/Management/SettingsSwitcher.cs
@@ -6,6 +6,30 @@ public class SettingsSwitcher : MonoBehaviour
 {
     public GameObject videoSettings;
     public GameObject comfortSettings;
+    public GameObject menu;
+
+    private int count = 0;
+    
+    public void Awake()
+    {
+        EventManager.instance.OnUpdateVideoSettingsUI += UpdateCount;
+        EventManager.instance.OnUpdateComfortSettingsUI += UpdateCount;
+    }
+
+    private void UpdateCount()
+    {
+        count++;
+        Debug.Log("Count: " + count, this);
+
+        if (count == 2)
+        {
+            EventManager.instance.OnUpdateVideoSettingsUI -= UpdateCount;
+            EventManager.instance.OnUpdateComfortSettingsUI -= UpdateCount;
+            
+            ToggleComfortSettings();
+            menu.SetActive(false);
+        }
+    }
 
     public void ToggleVideoSettings()
     {
